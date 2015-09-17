8b99cb8dbc543de08c2ff14602bb48bdbd2603cb
Diego Alves Said Simão <diego@doublelogic.com.br>
2015-09-17 10:55:23 -0300

    initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by diegoalvessaidsimao on 17/09/15.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.print("Hello word");
+        System.out.print("Hello word");
+    }
+
+}
